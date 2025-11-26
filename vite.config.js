d0bba017f255e022f62a433a05614ca5c88d7d62
Shakeel Ahamed <shakeelahamed2610@gmail.com>
2025-11-26 17:28:37 +0530

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   // THIS IS CRITICAL FOR GITHUB PAGES
   base: "/my-portfolio/",
 });
